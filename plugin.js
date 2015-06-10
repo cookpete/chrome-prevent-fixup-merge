0bd4ef8d91788e5e264e09b6a400151b43062bf2
Pete Cook <pete@cookpete.com>
2015-06-10 15:34:32 +0100

    First commit

---
+++ plugin.js
@@ -0,0 +1,12 @@
+var nodes = document.querySelectorAll('.commit-message .message');
+
+for(var i = 0, l = nodes.length; i !== l; i++){
+  if(/^fixup!/.test(nodes[i].innerText)) {
+    var button = document.querySelector('.merge-branch-action');
+    button.setAttribute('disabled', 'disabled');
+    button.classList.remove('btn-primary');
+    document.querySelector('.merge-branch-heading').innerText = 'Squash fixups before merging.';
+    document.querySelector('.merge-branch-description').innerHTML = '<code>git rebase origin/master -i --autosquash</code>';
+    break;
+  }
+}
