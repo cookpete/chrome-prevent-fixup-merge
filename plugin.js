495b7f147b307b5f02fc08b45bcd17d24d8e67ae
Pete Cook <pete@cookpete.com>
2015-07-06 13:38:43 +0100

    Refactor to show an alert rather than disable the button
    Because github loads PRs client-side, the script wasn't working properly before

--- plugin.js
+++ plugin.js
@@ -1,12 +1,20 @@
-var nodes = document.querySelectorAll('.commit-message .message');
-
-for(var i = 0, l = nodes.length; i !== l; i++){
-  if(/^(fixup|squash|wip|tmp)!/.test(nodes[i].innerText)) {
-    var button = document.querySelector('.merge-branch-action');
-    button.setAttribute('disabled', 'disabled');
-    button.classList.remove('btn-primary');
-    document.querySelector('.merge-branch-heading').innerText = 'Squash fixups before merging.';
-    document.querySelector('.merge-branch-description').innerHTML = '<code>git rebase origin/master -i --autosquash</code>';
-    break;
+window.addEventListener('click', function(e){
+  var node = e.target;
+  while(node){
+    if(node.classList && node.classList.contains('merge-branch-action') && hasFixups()) {
+      alert('Warning: You still have fixup commits');
+      break;
+    }
+    node = node.parentNode;
   }
+});
+
+function hasFixups(){
+  var nodes = document.querySelectorAll('.commit-message .message');
+  for(var i = 0, l = nodes.length; i !== l; i++){
+    if(/^(fixup|squash|wip|tmp)!/.test(nodes[i].innerText)) {
+      return true;
+    }
+  }
+  return false;
 }
